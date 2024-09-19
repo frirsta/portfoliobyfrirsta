13e99145f3560bbab7437803e43809bd48f40a0c
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-09-19 11:11:08 +0200

    Fix contact form success function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -92,7 +92,7 @@ const Contact = () => {
                 SEND MESSAGE
               </Button>
             </HoverScale>
-            {status === "ok" && error !== null && (
+            {status === "ok" && (
               <div className="alert alert-success">
                 <ThankYouAnimation onClose={() => setError(null)} />
               </div>
@@ -112,4 +112,38 @@ const Contact = () => {
   );
 };
 
+function SuccessIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-current shrink-0 h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+function ErrorIcon(success) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-current shrink-0 h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
 export default Contact;
